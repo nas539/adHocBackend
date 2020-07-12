aa042d6a7dd83fd90468f91f8fa864891a3c9e7c
Nicholas Sharp <naas8905@gmail.com>
2020-07-12 13:27:19 -0700

    Creating user table and endpoints

--- app.py
+++ app.py
@@ -16,6 +16,43 @@ heroku = Heroku(app)
 CORS(app)
 bcrypt = Bcrypt(app)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(), nullable=False)
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "username", "password")
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+@app.route("/user/add", methods=["POST"])
+def create_user():
+    if request.content_type != "application/json":
+        return jsonify("Error")
+
+    post_data = request.get_json()
+    username = post_data.get("username")
+    password = post_data.get("password")
+
+    username_check = db.session.query(User.username).filter(User.username == username).first()
+    if username_check is not None:
+        return jsonify("Username taken")
+
+    hashed_password = bcrypt.generate_password_hash(password).decode("utf8")
+
+    record = User(username, hashed_password)
+    db.session.add(record)
+    db.session.commit()
+
+    return jsonify("User created")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
